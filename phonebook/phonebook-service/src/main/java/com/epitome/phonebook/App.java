987e7baf2aef0c8cd6fb88907ea065bc30d37909
Amar Nandigam <amarendra.nandigam@gmail.com>
2012-08-04 00:50:50 -0400

    created service layer and ear layers

---
+++ phonebook/phonebook-service/src/main/java/com/epitome/phonebook/App.java
@@ -0,0 +1,13 @@
+package com.epitome.phonebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
