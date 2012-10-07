c397ba67b30a7672833cd86869cda7e91add8495
Amar Nandigam <amarendra.nandigam@gmail.com>
2012-10-06 21:55:53 -0400

    Changed everything to CDI now

--- phonebook/phonebook-web/src/main/java/com/epitome/phonebook/web/ContactListBacking.java
+++ phonebook/phonebook-web/src/main/java/com/epitome/phonebook/web/ContactListBacking.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -14,32 +13,40 @@ import org.slf4j.LoggerFactory;
 import com.epitome.phonebook.application.ContactService;
 import com.epitome.phonebook.domain.Contact;
 
-@Named
+@Named("clb")
 @RequestScoped
 public class ContactListBacking {
 	private static Logger log = LoggerFactory.getLogger(ContactListBacking.class);
+	
 	@Inject
 	private ContactService contactService;
 
 	private Contact searchCriteria = new Contact();
 	private List<Contact> searchResult = new ArrayList<Contact>();
 
-	@Produces
-	@Named
-	@RequestScoped
 	public Contact getSearchCriteria() {
 		return searchCriteria;
 	}
 	
-	@Produces
-	@Named
-	@RequestScoped
+	
 	public List<Contact> getSearchResult() {
 		return searchResult;
 	}
 
+	public String getMessage() {
+		return "Hello CDI!";
+	}
+	
 	public String processContactSearch() {
-		contactService.search(searchCriteria);
+		log.debug("Processing contactSearch");
+		//searchResult = contactService.search(searchCriteria);
+		List<Contact> contacts = new ArrayList<Contact>();
+		Contact contact = new Contact();
+		contact.setFirstName("Amar");
+		contact.setLastName("Nandigam");
+		contact.setIdentity(1);
+		contacts.add(contact);
+		searchResult = contacts;
 		return null;
 	}
 
